ccb047453e959b28ff0e6b844491bb843b6ab27b
Robocoders <team@blackbox.ai>
2024-11-20 23:59:37 +0000

    Update

--- new_sweet_flowers/src/components/Header.js
+++ new_sweet_flowers/src/components/Header.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
 
-function Header() {
+const Header = ({ isAuthenticated, onLogout }) => {
     return (
         <header>
-            <nav>
-                <div className="logo">Sweet Flowers</div>
-                <ul>
-                    <li><a href="#home">Головна</a></li>
-                    <li><a href="#about">Про нас</a></li>
-                    <li><a href="#products">Продукція</a></li>
-                    <li><a href="#contacts">Контакти</a></li>
-                </ul>
-            </nav>
+            <div className="header-content">
+                <div className="logo">
+                    <Link to="/">Sweet Flowers</Link>
+                </div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Головна</Link></li>
+                        <li><Link to="/products">Букети</Link></li>
+                        <li><Link to="/about">Про нас</Link></li>
+                        <li><Link to="/contacts">Контакти</Link></li>
+                        {isAuthenticated ? (
+                            <>
+                                <li><Link to="/admin">Адмін-панель</Link></li>
+                                <li><button onClick={onLogout}>Вийти</button></li>
+                            </>
+                        ) : (
+                            <li><Link to="/login">Увійти</Link></li>
+                        )}
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
-}
+};
 
 export default Header;
