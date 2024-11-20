e78bf8f72343b869e0274cb00e63c184e502d8d3
Robocoders <team@blackbox.ai>
2024-11-20 22:22:35 +0000

    Update

---
+++ new_sweet_flowers/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2023 Sweet Flowers. Всі права захищені.</p>
+        </footer>
+    );
+}
+
+export default Footer;
